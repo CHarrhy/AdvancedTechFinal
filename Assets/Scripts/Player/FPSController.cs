bb700736269aae25b681bafd4723a35fde520ede
Chris <christian2.harrhy@live.uwe.ac.uk>
2023-12-10 18:11:52 +0000

    Quit working

--- Assets/Scripts/Player/FPSController.cs
+++ Assets/Scripts/Player/FPSController.cs
@@ -261,6 +261,10 @@ public class FPSController : MonoBehaviour
 
     public void QuitGame()
     {
+#if UNITY_EDITOR
+        UnityEditor.EditorApplication.isPlaying = false;
+#else
         Application.Quit();
+#endif
     }
 }
