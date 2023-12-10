46d8fc1786344c3753e34787babcc2d965dcafef
Chris <christian2.harrhy@live.uwe.ac.uk>
2023-12-10 18:52:02 +0000

    Pause menu working fully

---
+++ Assets/Scripts/UI/PauseManager.cs
@@ -0,0 +1,77 @@
+using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.EventSystems;
+
+public class PauseManager : MonoBehaviour
+{
+    public GameObject pauseMenu;
+    public Button resumeButton;
+    public Button quitButton;
+
+    private bool isPaused = false;
+
+    void Start()
+    {
+        Cursor.lockState = CursorLockMode.Locked;
+        Cursor.visible = false;
+
+        resumeButton.onClick.AddListener(Resume);
+        quitButton.onClick.AddListener(Quit);
+    }
+
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.P))
+        {
+            TogglePause();
+        }
+
+        if (isPaused)
+        {
+            HandleMenuNavigation();
+        }
+    }
+
+    void TogglePause()
+    {
+        isPaused = !isPaused;
+
+        if (isPaused)
+        {
+            Time.timeScale = 0f;
+            Cursor.lockState = CursorLockMode.None;
+            Cursor.visible = true;
+            pauseMenu.SetActive(true);
+            EventSystem.current.SetSelectedGameObject(resumeButton.gameObject);
+        }
+        else
+        {
+            Time.timeScale = 1f;
+            Cursor.lockState = CursorLockMode.Locked;
+            Cursor.visible = false;
+            pauseMenu.SetActive(false);
+        }
+    }
+
+    void HandleMenuNavigation()
+    {
+        if (EventSystem.current.currentSelectedGameObject == null)
+        {
+            EventSystem.current.SetSelectedGameObject(resumeButton.gameObject);
+        }
+    }
+
+    void Resume()
+    {
+        TogglePause();
+    }
+
+    void Quit()
+    {
+#if UNITY_EDITOR
+        UnityEditor.EditorApplication.isPlaying = false;
+#else
+    Application.Quit();
+#endif
+    }
+}
