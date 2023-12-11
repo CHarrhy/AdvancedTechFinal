397c90b70c58a4437fe2fae20f4f72ce80103e53
TheHighWhale <chris.harrhy@hotmail.co.uk>
2023-12-11 04:08:41 +0000

    update, remaining enemies still buggy

--- Assets/Scripts/UI/RemainingEnemiesUI.cs
+++ Assets/Scripts/UI/RemainingEnemiesUI.cs
@@ -1,32 +1,22 @@
-using UnityEngine;
 using TMPro;
+using UnityEngine;
 
 public class RemainingEnemiesUI : MonoBehaviour
 {
     public TextMeshProUGUI remainingEnemiesText;
 
-    void Start()
-    {
-        // Ensure the Text component is assigned
-        if (remainingEnemiesText == null)
-        {
-            Debug.LogError("RemainingEnemiesUI: Text component is not assigned!");
-            return;
-        }
-
-        // Initialize the text with the starting number of enemies
-        UpdateRemainingEnemiesCount(EnemyManager.Instance.RemainingEnemies);
-    }
-
     void Update()
     {
-        // Update the text with the remaining enemies count
-        UpdateRemainingEnemiesCount(EnemyManager.Instance.RemainingEnemies);
+        // Update the remaining enemies text
+        UpdateRemainingEnemiesText();
     }
 
-    void UpdateRemainingEnemiesCount(int count)
+    void UpdateRemainingEnemiesText()
     {
-        // Update the TextMeshPro Text with the remaining enemies count
-        remainingEnemiesText.text = "Remaining Enemies: " + count.ToString();
+        // Get the remaining enemies count from the EnemyManager
+        int remainingEnemies = EnemyManager.Instance.RemainingEnemies;
+
+        // Update the UI text
+        remainingEnemiesText.text = "Remaining Enemies: " + remainingEnemies;
     }
 }
